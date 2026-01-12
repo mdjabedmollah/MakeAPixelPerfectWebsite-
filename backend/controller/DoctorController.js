5aa43c14452ec0f0799cfd8569452406d58a634c
mdjabedmollah <mdjabed2167@gmail.com>
2026-01-12 20:16:41 +0600

    get all doctor

--- backend/controller/DoctorController.js
+++ backend/controller/DoctorController.js
@@ -1,16 +1,9 @@
 import Doctor from "../model/DoctorModel.js";
 
-
-const AdDataDoctor = async (req, res) => {
+export const AdDataDoctor = async (req, res) => {
   try {
-    const {
-      name,
-      degree,
-      registrationNumber,
-      experience,
-      status,
-      image,
-    } = req.body;
+    const { name, degree, registrationNumber, experience, status, image } =
+      req.body;
 
     //  Validation
     if (!name || !degree || !registrationNumber || !experience) {
@@ -53,4 +46,46 @@ const AdDataDoctor = async (req, res) => {
   }
 };
 
-export default AdDataDoctor;
+export const getAllDoctors = async (req, res) => {
+  try {
+    const doctors = await Doctor.find();
+
+    return res.status(200).json({
+      success: true,
+      count: doctors.length,
+      data: doctors,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+export const getOneDoctor = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const doctor = await Doctor.findById(id);
+
+   
+    if (!doctor) {
+      return res.status(404).json({
+        success: false,
+        message: "Doctor not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: doctor,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,8 @@
 import express from 'express'
-import AdDataDoctor from '../controller/DoctorController.js'
+import {AdDataDoctor, getAllDoctors, getOneDoctor } from '../controller/DoctorController.js'
 const route =express.Router()
 route.post('/doctor',AdDataDoctor)
+route.get('/doctor',getAllDoctors)
+
+route.get("/doctor/:id", getOneDoctor);
 export default route
